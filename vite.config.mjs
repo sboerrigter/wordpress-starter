5f3e0f43f3ddb9f78c65dcc82ead7cfa39e85fb9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-21 08:07:20 +0100

    🔧 Vite config, inspired by https://github.com/vinkla/wordplate/blob/master/vite.config.js

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,24 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  build: {
+    assetsDir: '',
+    emptyOutDir: true,
+    manifest: true,
+    outDir: `web/wp-content/themes/starter/dist`,
+    rollupOptions: {
+      main: 'web/wp-content/themes/starter/scripts/main.js',
+    },
+  },
+  plugins: [
+    {
+      name: 'php',
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith('.php')) {
+          server.ws.send({ type: 'full-reload', path: '*' });
+        }
+      },
+    },
+    tailwindcss(),
+  ],
 });
