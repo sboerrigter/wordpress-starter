336c713d32d7331817316a7a9d093ef1fa0f41c5
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-25 12:36:58 +0100

    ♻️ application.php and .env.example

--- application.php
+++ application.php
@@ -7,24 +7,10 @@ use function Env\env;
 // USE_ENV_ARRAY + CONVERT_* + STRIP_QUOTES
 Env\Env::$options = 31;
 
-/**
- * Directory containing all of the site's files
- *
- * @var string
- */
 $root_dir = __DIR__;
-
-/**
- * Document Root
- *
- * @var string
- */
 $webroot_dir = $root_dir . '/web';
 
-/**
- * Use Dotenv to set required environment variables and load .env file in root
- * .env.local will override .env if it exists
- */
+// Define required environment variables and load .env file
 if (file_exists($root_dir . '/.env')) {
   $env_files = file_exists($root_dir . '/.env.local')
     ? ['.env', '.env.local']
@@ -39,37 +25,24 @@ if (file_exists($root_dir . '/.env')) {
   $dotenv = Dotenv\Dotenv::create($repository, $root_dir, $env_files, false);
   $dotenv->load();
 
-  $dotenv->required(['WP_HOME', 'WP_SITEURL']);
-  if (!env('DATABASE_URL')) {
-    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
-  }
+  $dotenv->required(['DB_NAME', 'DB_PASSWORD', 'DB_USER', 'WP_ENV', 'WP_HOME']);
 }
 
-/**
- * Set up our global environment constant and load its config first
- * Default: production
- */
+// Define environment variable and load its config first
 define('WP_ENV', env('WP_ENV') ?: 'production');
 
-/**
- * Infer WP_ENVIRONMENT_TYPE based on WP_ENV
- */
-if (
-  !env('WP_ENVIRONMENT_TYPE') &&
-  in_array(WP_ENV, ['production', 'staging', 'development', 'local'])
-) {
-  Config::define('WP_ENVIRONMENT_TYPE', WP_ENV);
-}
-
-/**
- * URLs
- */
+// WordPress
+Config::define('AUTOMATIC_UPDATER_DISABLED', true);
+Config::define('CONCATENATE_SCRIPTS', false);
+Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+Config::define('DISALLOW_FILE_EDIT', true);
+Config::define('DISALLOW_FILE_MODS', true);
+Config::define('WP_ENVIRONMENT_TYPE', WP_ENV);
 Config::define('WP_HOME', env('WP_HOME'));
-Config::define('WP_SITEURL', env('WP_SITEURL'));
+Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?? true);
+Config::define('WP_SITEURL', env('WP_HOME') . '/wp');
 
-/**
- * Custom Content Directory
- */
+// Content directory
 Config::define('CONTENT_DIR', '/wp-content');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
 Config::define(
@@ -77,13 +50,7 @@ Config::define(
   Config::get('WP_HOME') . Config::get('CONTENT_DIR')
 );
 
-/**
- * DB settings
- */
-if (env('DB_SSL')) {
-  Config::define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
-}
-
+// Database
 Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
 Config::define('DB_NAME', env('DB_NAME'));
 Config::define('DB_USER', env('DB_USER'));
@@ -92,21 +59,14 @@ Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
 
-if (env('DATABASE_URL')) {
-  $dsn = (object) parse_url(env('DATABASE_URL'));
+// Debuggingg
+Config::define('WP_DEBUG', env('WP_DEBUG') ?? false);
+Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?? false);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
+Config::define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?? false);
+ini_set('display_errors', '0');
 
-  Config::define('DB_NAME', substr($dsn->path, 1));
-  Config::define('DB_USER', $dsn->user);
-  Config::define('DB_PASSWORD', isset($dsn->pass) ? $dsn->pass : null);
-  Config::define(
-    'DB_HOST',
-    isset($dsn->port) ? "{$dsn->host}:{$dsn->port}" : $dsn->host
-  );
-}
-
-/**
- * Authentication Unique Keys and Salts
- */
+// Authentication
 Config::define('AUTH_KEY', env('AUTH_KEY'));
 Config::define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
 Config::define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
@@ -116,37 +76,7 @@ Config::define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
 Config::define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
 Config::define('NONCE_SALT', env('NONCE_SALT'));
 
-/**
- * Custom Settings
- */
-Config::define('AUTOMATIC_UPDATER_DISABLED', true);
-Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
-
-// Disable the plugin and theme file editor in the admin
-Config::define('DISALLOW_FILE_EDIT', true);
-
-// Disable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', true);
-
-// Limit the number of post revisions
-Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?? true);
-
-// Disable script concatenation
-Config::define('CONCATENATE_SCRIPTS', false);
-
-/**
- * Debugging Settings
- */
-Config::define('WP_DEBUG', env('WP_DEBUG') ?? false);
-Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?? false);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
-Config::define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?? false);
-ini_set('display_errors', '0');
-
-/**
- * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
- * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
- */
+// Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
 if (
   isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
   $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
@@ -154,17 +84,9 @@ if (
   $_SERVER['HTTPS'] = 'on';
 }
 
-$env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
-
-if (file_exists($env_config)) {
-  require_once $env_config;
-}
-
 Config::apply();
 
-/**
- * Bootstrap WordPress
- */
+// Bootstrap WordPress
 if (!defined('ABSPATH')) {
   define('ABSPATH', $webroot_dir . '/wp/');
 }
