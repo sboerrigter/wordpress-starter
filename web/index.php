86e8727f1fb5e727a8a30e51c051567ca5553dcb
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-25 12:37:36 +0100

    🔧 Redirect /wp-admin and /wp-login.php to WordPress

--- web/index.php
+++ web/index.php
@@ -1,3 +1,9 @@
 <?php
+// Redirect /wp-admin and /wp-login.php to WordPress
+if (strpos($uri, '/wp-admin') === 0 || strpos($uri, '/wp-login.php') === 0) {
+  header("Location: /wp{$uri}");
+  exit();
+}
+
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
