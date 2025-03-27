28f53c48bdfbbfaae60b279e86e45e0e50d54b03
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-27 22:07:17 +0100

    💄 Image background color

--- web/wp-content/themes/custom/components/image.php
+++ web/wp-content/themes/custom/components/image.php
@@ -6,7 +6,7 @@ $image = new Image($args);
 
 <img
     alt="<?= $image->alt ?>"
-    class="<?= $image->class ?>"
+    class="<?= $image->class ?> bg-gray-600"
     height="<?= $image->height ?>"
     loading="<?= $image->loading ?>"
     sizes="<?= $image->sizes ?>"
