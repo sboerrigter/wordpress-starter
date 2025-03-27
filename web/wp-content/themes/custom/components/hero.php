8cbece7895750b47e06a43d664161bfc1bdb29d7
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-27 22:30:16 +0100

    Show line if there is no header

--- web/wp-content/themes/custom/components/hero.php
+++ web/wp-content/themes/custom/components/hero.php
@@ -1,9 +1,13 @@
 <header class="wrapper">
-  <?= component('image', [
-    'class' => 'rounded-lg max-h-[600px] object-cover',
-    'src' => get_field('_thumbnail_id'),
-    'width' => 1200,
-    'height' => 900,
-    'widths' => [335, 480, 600, 1200],
-  ]) ?>
+  <?php if ($image = get_field('_thumbnail_id')) { ?>
+    <?= component('image', [
+      'class' => 'rounded-lg max-h-[600px] object-cover',
+      'src' => $image,
+      'width' => 1200,
+      'height' => 900,
+      'widths' => [335, 480, 600, 1200],
+    ]) ?>
+  <?php } else { ?>
+    <hr class="my-0" />
+  <?php } ?>
 </header>
