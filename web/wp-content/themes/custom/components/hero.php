a99261f6b50f19be983031ddb619435aabb91b2f
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-27 21:52:18 +0100

    Update hero.php

--- web/wp-content/themes/custom/components/hero.php
+++ web/wp-content/themes/custom/components/hero.php
@@ -1,6 +1,6 @@
 <header class="wrapper">
   <?= component('image', [
-    'class' => 'rounded-lg max-h-[400px] object-cover',
+    'class' => 'rounded-lg max-h-[600px] object-cover',
     'src' => get_field('_thumbnail_id'),
     'width' => 1200,
     'height' => 900,
