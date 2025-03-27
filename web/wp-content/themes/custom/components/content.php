74d6b80d77764dd855da29ebb12ecb098125a934
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-03-27 23:21:22 +0100

    Update content.php

--- web/wp-content/themes/custom/components/content.php
+++ web/wp-content/themes/custom/components/content.php
@@ -1,5 +1,8 @@
 <section>
-  <div class="wrapper max-w-[780px]">
+  <div class="
+    content wrapper max-w-[760px] mt-10 mb-15
+    md:mt-20 md:mb-25
+  ">
     <h1><?php the_title(); ?></h1>
 
     <?php the_content(); ?>
