e3383ef5c01206f7e9ef2dd40f2a8f071646ad2c
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-04-04 16:00:24 +0200

    Update logo.php

--- web/wp-content/themes/theme/components/logo.php
+++ web/wp-content/themes/theme/components/logo.php
@@ -1,4 +1,7 @@
-<a href="<?= home_url() ?>" class="text-xl text-gray-800 font-bold no-underline flex items-center gap-3">
+<a
+    href="<?= home_url() ?>"
+    class="text-xl text-gray-800 font-bold no-underline flex items-center gap-3"
+>
     <div class="w-8 h-8 rounded-full bg-primary-600"></div>
     <?= get_bloginfo('sitename') ?>
 </a>
