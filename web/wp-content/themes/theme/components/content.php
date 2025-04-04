dd83c27126368cc1ac63a9c3a8cadb0ef5f6a852
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-04-04 20:06:42 +0200

    💄 Content width

--- web/wp-content/themes/theme/components/content.php
+++ web/wp-content/themes/theme/components/content.php
@@ -1,6 +1,6 @@
 <section>
   <div class="
-    content wrapper max-w-[760px] mt-10 mb-15
+    content wrapper max-w-content mt-10 mb-15
     md:mt-20 md:mb-25
   ">
     <h1><?php the_title(); ?></h1>
