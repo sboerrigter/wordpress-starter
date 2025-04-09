d404198b3e17f10bb5c2ec7c9832fca817e8d35e
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-04-09 20:35:19 +0200

    🚧 separator block

--- web/wp-content/themes/theme/scripts/editor.js
+++ web/wp-content/themes/theme/scripts/editor.js
@@ -2,11 +2,7 @@ wp.domReady(function () {
   // Unregister block styles
   wp.blocks.unregisterBlockStyle('core/image', 'default');
   wp.blocks.unregisterBlockStyle('core/image', 'rounded');
-
-  wp.blocks.unregisterBlockStyle('core/separator', 'default');
-  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
-  wp.blocks.unregisterBlockStyle('core/separator', 'wide');
-
   wp.blocks.unregisterBlockStyle('core/quote', 'default');
   wp.blocks.unregisterBlockStyle('core/quote', 'plain');
+  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
 });
